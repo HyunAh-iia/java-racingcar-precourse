a452b72e4fe1bc6593a9ab4b109715e7e5504f38
iia <kha2464@gmail.com>
2021-10-11 22:18:07 +0900

    test: 경주를 진행 시, 랜덤값에 따른 자동차 전진 검증 추가

--- src/test/java/racinggame/domain/RacingCarTest.java
+++ src/test/java/racinggame/domain/RacingCarTest.java
@@ -2,14 +2,23 @@ package racinggame.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import nextstep.test.NSTest;
+import racinggame.Application;
 import racinggame.wrapper.CarName;
 
-public class RacingCarTest {
+public class RacingCarTest extends NSTest {
+
+	@BeforeEach
+	void beforeEach() {
+		setUp();
+	}
 
 	@DisplayName("올바른 이름을 넣었을 때 자동차 생성 검증")
 	@Test
@@ -26,5 +35,27 @@ public class RacingCarTest {
 		assertThatIllegalArgumentException().isThrownBy(() ->
 			new RacingCar(new CarName(input))
 		);
+	}
+
+	@DisplayName("경주 자동차의 전진 검증")
+	@Test
+	void racingCarMoveTest() {
+		int go = 5;
+		int stop = 0;
+
+		assertRandomTest(() -> {
+			run("one,two", "1");
+			verify("one : -", "two :"); // fix me "two :" -> "two : "
+		}, go, stop);
+	}
+
+	@AfterEach
+	void tearDown() {
+		outputStandard();
+	}
+
+	@Override
+	public void runMain() {
+		Application.main(new String[] {});
 	}
 }
