c681243521354bd80d3d5f15205b3dfa3ae15920
iia <kha2464@gmail.com>
2021-10-11 23:45:17 +0900

    refactor: for loop를 enhanced for로 변경하고 미사용 변수 삭제

--- src/main/java/racinggame/view/Player.java
+++ src/main/java/racinggame/view/Player.java
@@ -12,7 +12,6 @@ public class Player {
 	private static final String MESSAGE_WRITE_DOWN_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String MESSAGE_WRITE_DOWN_ROUND_COUNT = "시도할 회수는 몇회인가요?";
 	private static final String MESSAGE_ROUND_COUNT_ONLY_POSITIVE = "시도 회수는 양의 정수만 입력 가능합니다.";
-	private static final String MESSAGE_MUST_WRITE = "";
 	private static final String SEPARATOR = ",";
 
 	public static List<CarName> writeDownCarNames() {
@@ -38,8 +37,8 @@ public class Player {
 	private static List<CarName> validateCarNames(String carNames) {
 		String[] separatedNames = carNames.split(SEPARATOR, -1);
 		List<CarName> carNameList = new ArrayList<>();
-		for (int i = 0; i < separatedNames.length; i++) {
-			carNameList.add(new CarName(separatedNames[i]));
+		for (String separatedName : separatedNames) {
+			carNameList.add(new CarName(separatedName));
 		}
 		return carNameList;
 	}
