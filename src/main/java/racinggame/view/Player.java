9a6cda5eae87e54566fdc19fe093de5aef36e000
iia <kha2464@gmail.com>
2021-10-11 20:33:29 +0900

    refactor: 자동차 이름을 나타내는 변수를 포장

--- src/main/java/racinggame/view/Player.java
+++ src/main/java/racinggame/view/Player.java
@@ -1,57 +1,53 @@
 package racinggame.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import nextstep.utils.Console;
+import racinggame.wrapper.CarName;
 
 public class Player {
 	private static final String ERROR_PREFIX = "[ERROR] ";
-	private static final String WRITE_DOWN_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-	private static final String WRITE_DOWN_ROUND_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
-	private static final String WRONG_CAR_NAMES_ERROR_MESSAGE = ERROR_PREFIX + "자동차 이름은 쉼표로 구분하며, 5자 이하만 입력할 수 있습니다.";
-	private static final String WRONG_ROUND_COUNT_ERROR_MESSAGE = ERROR_PREFIX + "시도 회수는 양의 정수만 입력 가능합니다.";
+	private static final String MESSAGE_WRITE_DOWN_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String MESSAGE_WRITE_DOWN_ROUND_COUNT = "시도할 회수는 몇회인가요?";
+	private static final String MESSAGE_ROUND_COUNT_ONLY_POSITIVE = "시도 회수는 양의 정수만 입력 가능합니다.";
+	private static final String MESSAGE_MUST_WRITE = "";
 	private static final String SEPARATOR = ",";
-	private static final int MAX_CAR_NAME = 5;
 
-	public static String[] writeDownCarNames() {
-		System.out.println(WRITE_DOWN_CAR_NAMES_MESSAGE);
+	public static List<CarName> writeDownCarNames() {
+		System.out.println(MESSAGE_WRITE_DOWN_CAR_NAMES);
 		try {
 			return validateCarNames(Console.readLine());
 		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
+			System.out.println(ERROR_PREFIX + e.getMessage());
 			return writeDownCarNames();
 		}
 	}
 
 	public static int writeDownRoundCount() {
-		System.out.println(WRITE_DOWN_ROUND_COUNT_MESSAGE);
+		System.out.println(MESSAGE_WRITE_DOWN_ROUND_COUNT);
 		try {
 			return validateRoundCount(Console.readLine());
 		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
+			System.out.println(ERROR_PREFIX + e.getMessage());
 			return writeDownRoundCount();
 		}
 	}
 
-	private static String[] validateCarNames(String carNames) {
-		String[] separatedNames = carNames.split(SEPARATOR);
-		int validated = 0;
+	private static List<CarName> validateCarNames(String carNames) {
+		String[] separatedNames = carNames.split(SEPARATOR, -1);
+		List<CarName> carNameList = new ArrayList<>();
 		for (int i = 0; i < separatedNames.length; i++) {
-			validated += isLengthLowerOrEqual(separatedNames[i]) ? 1 : 0;
+			carNameList.add(new CarName(separatedNames[i]));
 		}
-		if (validated != separatedNames.length) {
-			throw new IllegalArgumentException(WRONG_CAR_NAMES_ERROR_MESSAGE);
-		}
-		return separatedNames;
+		return carNameList;
 	}
 
 	private static int validateRoundCount(String roundCount) {
 		try {
 			return Integer.parseInt(roundCount);
 		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(WRONG_ROUND_COUNT_ERROR_MESSAGE);
+			throw new IllegalArgumentException(MESSAGE_ROUND_COUNT_ONLY_POSITIVE);
 		}
-	}
-
-	private static boolean isLengthLowerOrEqual(String carName) {
-		return !!(carName.length() <= MAX_CAR_NAME);
 	}
 }
---
+++ src/main/java/racinggame/wrapper/CarName.java
@@ -0,0 +1,22 @@
+package racinggame.wrapper;
+
+public class CarName {
+	private static final String MESSAGE_TOO_SHORT = "자동차의 이름은 한 글자 이상 입력하셔야합니다.";
+	private static final String MESSAGE_TOO_LONG = "자동차의 이름은 다섯 글자 이하로 입력하셔야합니다.";
+	private static final int MAX_CAR_NAME = 5;
+	private String carName;
+
+	public CarName(String carName) {
+		if (carName == null || carName.isEmpty()) {
+			throw new IllegalArgumentException(MESSAGE_TOO_SHORT);
+		}
+		if (carName.length() <= MAX_CAR_NAME) {
+			throw new IllegalArgumentException(MESSAGE_TOO_LONG);
+		}
+		this.carName = carName;
+	}
+
+	public String get() {
+		return this.carName;
+	}
+}
